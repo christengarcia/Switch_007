d204e9005e0a6fd9f025d0181cc3ae72e1942470
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-18 10:49:31 +1100

    First commit

---
+++ Switch_007/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Switch_007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pick a number between 1 and 6: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 6:
+                case 5:
+                    Console.WriteLine("You picked the biggest legal value");
+                    break;
+                case 4:
+                    Console.WriteLine("You chose 4");
+                    break;
+                case 3:
+                    Console.WriteLine("You chose 3");
+                    break;
+                case 2:
+                    Console.WriteLine("You chose 2");
+                    break;
+                case 1:
+                    Console.WriteLine("You picked the lowest legal value");
+                    break;
+                default:
+                    Console.WriteLine("Not a legal entry. Please try again");
+                    break;
+            }
+        }
+    }
+}
